416eab8fb72f7c063fe83c71e7d0c75152163425
DIVYANSH KUMAR <divyanshleet@gmail.com>
2023-07-22 05:51:07 +0530

    All statistical APIs added

--- controller/controller.statistics.js
+++ controller/controller.statistics.js
@@ -23,17 +23,68 @@ const totalSaleAmount = async (req, res) => {
     const totalSaleAmount = totalSaleAmount2021 + totalSaleAmount2022;
     res.json({ totalSaleAmount});
   } catch (err) {
-    console.error('Error fetching total sale amount:', err);
-    res.status(500).json({ error: 'Failed to fetch total sale amount.' });
+    console.error('There was an error fetching total sale amount:', err);
+    res.status(500).json({ error: 'Failed to fetch the total sale amount' });
   }
 };
 
 
+const totalSoldItems = async (req, res) => {
+  try {
+    const { month } = req.params;
 
+    // Convert month string to number
+    const selectedMonth = new Date(`${month} 1`).getMonth() + 1;
 
+    // Calculate the start and end dates for the selected month and for both years (2021 and 2022), as only these two years are present in the database
+    const startDate2021 = new Date(Date.UTC(2021, selectedMonth - 1, 1)).toISOString();
+    const endDate2021 = new Date(Date.UTC(2021, selectedMonth, 1)).toISOString();
+    const startDate2022 = new Date(Date.UTC(2022, selectedMonth - 1, 1)).toISOString();
+    const endDate2022 = new Date(Date.UTC(2022, selectedMonth, 1)).toISOString();
 
+    // Fetch sold products for both years and the specified month
+    const products2021 = await Product.find({ dateOfSale: { $gte: startDate2021, $lt: endDate2021 }, sold: true });
+    const products2022 = await Product.find({ dateOfSale: { $gte: startDate2022, $lt: endDate2022 }, sold: true });
 
+    // Calculate the total number of sold items for both years
+    const totalSoldItems = products2021.length + products2022.length;
+
+    res.json({ totalSoldItems });
+  } catch (err) {
+    console.error('There was an error fetching total sold items:', err);
+    res.status(500).json({ error: 'Failed to fetch the total sold items.' });
+  }
+};
+
+const totalNotSoldItems = async (req, res) => {
+  try {
+    const { month } = req.params;
+
+    // Convert month string to number
+    const selectedMonth = new Date(`${month} 1`).getMonth() + 1;
+
+    // Calculate the start and end dates for the selected month and for both years (2021 and 2022), as only these two years are present in the database
+    const startDate2021 = new Date(Date.UTC(2021, selectedMonth - 1, 1)).toISOString();
+    const endDate2021 = new Date(Date.UTC(2021, selectedMonth, 1)).toISOString();
+    const startDate2022 = new Date(Date.UTC(2022, selectedMonth - 1, 1)).toISOString();
+    const endDate2022 = new Date(Date.UTC(2022, selectedMonth, 1)).toISOString();
+
+    // Fetch not sold products for both years and the specified month
+    const products2021 = await Product.find({ dateOfSale: { $gte: startDate2021, $lt: endDate2021 }, sold: false });
+    const products2022 = await Product.find({ dateOfSale: { $gte: startDate2022, $lt: endDate2022 }, sold: false });
+
+    // Calculate the total number of not sold items for both years
+    const totalNotSoldItems = products2021.length + products2022.length;
+
+    res.json({ totalNotSoldItems });
+  } catch (err) {
+    console.error('There was an error fetching total not sold items:', err);
+    res.status(500).json({ error: 'Failed to fetch the total not sold items.' });
+  }
+};
 
 module.exports ={
-    totalSaleAmount
+    totalSaleAmount,
+    totalSoldItems,
+    totalNotSoldItems
 };
--- router/router.statistics.js
+++ router/router.statistics.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    totalSaleAmount
+    totalSaleAmount, totalSoldItems, totalNotSoldItems
 } = require('../controller/controller.statistics');
 
 router.get('/totalSaleAmount/:month', totalSaleAmount);
+router.get('/totalSoldItems/:month', totalSoldItems)
+router.get('/totalNotSoldItems/:month', totalNotSoldItems)
 
 module.exports = router;
